e33e6f7fb009cfd9fb14e7f2e4d47cee020f558a
Rayi_Yosoro <35384927+inewhero@users.noreply.github.com>
2024-03-06 18:42:25 +0800

    feat: async thumb loading

--- Sukebetter-JS.user.js
+++ Sukebetter-JS.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Sukebetter-JS
-// @homepage     https://github.com/inewhero/Sukebetter/tree/Sukebetter-JS
-// @version      0.1
+// @homepage     https://greasyfork.org/zh-CN/scripts/488024-sukebetter-js
+// @version      0.2
 // @description  Open images without annoying ads
 // @author       yosoro
+// @grant        GM_xmlhttpRequest
+// @match        *://sukebei.nyaa.si/*
 // @match        *://javball.com/*
 // @match        *://ovabee.com/*
 // @match        *://cnxx.me/*
@@ -13,7 +15,15 @@
 // @match        *://cnpics.org/*
 // @match        *://cosplay18.pics/*
 // @match        *://pig69.com/*
-// @match        *://sukebei.nyaa.si/*
+// @connect      *://javball.com/*
+// @connect      *://ovabee.com/*
+// @connect      *://cnxx.me/*
+// @connect      *://ai18.pics/*
+// @connect      *://porn4f.com/*
+// @connect      *://idol69.net/*
+// @connect      *://cnpics.org/*
+// @connect      *://cosplay18.pics/*
+// @connect      *://pig69.com/*
 // @run-at       document-end
 // @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.7.1/jquery.min.js
 // ==/UserScript==
@@ -27,12 +37,12 @@
         for (let i = 0; i < fcscripts.length; i++) {
             if (fcscripts[i].attributes.src) {
                 if (fcscripts[i].attributes.src.value.search(/min/i) == -1){
-                    console.log('remove ad scripts:' + fcscripts[i].attributes.src.value);
+                    console.log('remove ad scripts: ' + fcscripts[i].attributes.src.value);
                     fcscripts[i].remove();
                 }
             } else if (fcscripts[i].text) {
                 if (fcscripts[i].text.search(/adConfig/i) != -1){
-                    console.log('remove ad scripts:' + fcscripts[i].text.value);
+                    console.log('remove ad scripts: ' + fcscripts[i].text.value);
                     fcscripts[i].remove();
                 }
             }
@@ -50,18 +60,42 @@
 
         //sukebei.nyaa.si thumbnails detect
         if (window.location.pathname.includes('view')) {
+            let thumbs = [];
             let detail_elements = document.getElementById('torrent-description').innerHTML;
             let thumb_regex = /<a href="([^"]+\_s.jpg)"[^>]*>/g;
             let thumb_addr;
             while ((thumb_addr = thumb_regex.exec(detail_elements)) != null) {
-                window.open(thumb_addr[1], "_blank");
+                thumbs.push(thumb_addr[1]);
             }
+
+            //picture async load
+            let thumb_parent = document.querySelector('#torrent-description > hr:nth-child(4) + p');
+            thumbs.forEach(url => {
+                GM_xmlhttpRequest({
+                    method: "GET",
+                    url: url,
+                    responseType: "blob",
+                    onload: function(response) {
+                        let parser = new DOMParser();
+                        let doc = parser.parseFromString(response.responseText, 'text/html');
+                        let realimg = doc.getElementsByTagName('meta')['twitter:image:src'].content;
+                        let real_thumb = document.createElement('img');
+                        real_thumb.src = realimg;
+                        console.log('fetch thumb: ' + realimg);
+                        thumb_parent.innerHTML = "";
+                        thumb_parent.appendChild(real_thumb);
+                    },
+                    onerror: function(error) {
+                        console.error("GET error: ", error);
+                    }
+                });
+            });
         }
+
 
     //picture beds ad remove
     } else {
         let realimg=document.getElementsByTagName('meta')['twitter:image:src'].content;
-        console.log(realimg);
         window.open(realimg,'_self');
     }
 })();
